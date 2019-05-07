b7e219c644d7acce68655977615d18e70e2e2aed
Mateus Exel <mateus@exel.com.br>
2019-05-07 17:29:44 -0300

    interaction1 v2

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import Table from './compo/Table';
+import contacts from './contacts.json';
 
 class App extends Component {
-  // constructor(){
-  //   super();
-  //   this.name;
-  //   this.pictureUrl;
-  //   this.popularity;
-  // }   
+  constructor(){
+    super();
+    this.state = {
+      contactsList: contacts.slice(0,5)
+    }
+  }   
   
   render() {
     return (
       <div className="App">
         <h1 className="App-title">Ironhack Contacts</h1>
         <table>
-          <tr>
-            <th>Picture</th>
-            <th>Name</th>
-            <th>Popularity</th>
-          </tr>
-          <Table />
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.contactsList.map((e,idx) => {
+              return <Table contact={e} key={idx}/>
+            })}
+          </tbody>
         </table>
       </div>
     );
--- starter-code/src/compo/Table.js
+++ starter-code/src/compo/Table.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import contacts from '../contacts.json';
 import './table.css';
 
-const Table = () => {
-  const arr = []
-  for(let i = 0; i < 5; i += 1){
-   arr.push(<tr>
-          <th><img className="img"  src={contacts[i].pictureUrl} alt="img"></img></th>
-          <th>{contacts[i].name}</th>
-          <th>{contacts[i].popularity}</th>
-         </tr>)
-  }
-  return arr
+const Table = (props) => {
+  return (
+      <tr>
+        <th><img className="img"  src={props.contact.pictureUrl} alt="img"></img></th>
+        <th>{props.contact.name}</th>
+        <th>{props.contact.popularity}</th>
+      </tr>
+  )
 }
 export default Table;
